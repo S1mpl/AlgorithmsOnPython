08fb6371f36156d4281d9203a5354bc869fce6f8
S1mply <yl@open.by>
2017-03-09 13:13:05 +0300

    FindFactors

---
+++ NumericalAlgorithms/FindFactors.py
@@ -0,0 +1,26 @@
+import math
+
+def FindFactors(number):
+    factors = []
+
+    while number % 2 == 0:
+        factors.append(2)
+        number = number / 2
+
+    i = 3
+    max_factor = math.sqrt(number)
+
+    while i <= max_factor:
+        while number % i == 0:
+            factors.append(i)
+            number = number / i
+            max_factor = math.sqrt(number)
+        i = i + 2
+
+    if number > 1:
+        factors.append(number)
+
+    return factors
+
+#exampler
+print(FindFactors(12354123))
